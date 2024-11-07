c8c5eb342fae7c015a2e5ff199d6b51c1a693881
cesardarizaleta <141669141+cesardarizaleta@users.noreply.github.com>
2024-11-06 23:12:04 -0400

    Changing Color => Triangle

--- triangle.py
+++ triangle.py
@@ -1,16 +1,32 @@
 import matplotlib.pyplot as plt
 import random as R
+import numpy as np
 
 n = 10000
 
+# Coordenadas de los vértices del triángulo equilátero
 vertex_coordinates = [[-200.0, -100.0], [0.0, 200.0], [200.0, -100.0]]
+
+# Listas para almacenar las coordenadas de los puntos generados
 x_coordinates = [vertex_coordinates[0][0]]
 y_coordinates = [vertex_coordinates[0][1]]
+
+# Generar puntos aleatorios dentro del triángulo de Sierpinski
 for i in range(n):
     r = R.randrange(3)
     x_coordinates.append((x_coordinates[-1] + vertex_coordinates[r][0]) / 2)
     y_coordinates.append((y_coordinates[-1] + vertex_coordinates[r][1]) / 2)
+
+# Crear la figura y los ejes
 fig, ax = plt.subplots()
-ax.scatter(x_coordinates, y_coordinates, s = 0.1)
-ax.set(title=f'Sierpinski triangle for {n} points')
+
+#Por ejemplo se puede usar:
+# red, black, blue, green, yellow, cyan, magenta, white
+colors = 'black'  # +1 para incluir el primer punto
+
+# Dibujar los puntos con colores aleatorios
+ax.scatter(x_coordinates, y_coordinates, s=0.1, color=colors, alpha=0.5)
+
+# Configurar el título y mostrar el gráfico
+ax.set(title=f'Triángulo de Sierpinski para {n} puntos')
 plt.show()
